33c1efd7c29668ac3a010c49512515aa97d43b80
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2022-09-14 18:16:58 -0400

    cleaning up eslint warnings

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -1,16 +1,5 @@
 import React from "react";
-import { Fragment } from "react";
-import { Popover, Transition } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import Image from "next/image";
 import Navbar from "../Navbar";
-
-const navigation: [] = [
-  // { name: "Product", href: "#" },
-  // { name: "Features", href: "#" },
-  // { name: "Marketplace", href: "#" },
-  // { name: "Company", href: "#" },
-];
 
 type Props = {
   children: React.ReactNode;
